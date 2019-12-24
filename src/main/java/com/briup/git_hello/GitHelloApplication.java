19d6df449ddc3db108b3d4a00676c9f936a95e43
MJX99 <399895708@qq.com>
2019-12-24 15:17:08 +0800

    初始化

---
+++ src/main/java/com/briup/git_hello/GitHelloApplication.java
@@ -0,0 +1,13 @@
+package com.briup.git_hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitHelloApplication.class, args);
+    }
+
+}
